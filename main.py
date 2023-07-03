258e072b63a7fd76ef516f0b09afadf9fc1c7c61
Annanyabulchandani <137045909+Annanyabulchandani@users.noreply.github.com>
2023-07-03 17:58:04 +0530

    inital commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from time_calculator import add_time
+from unittest import main
+
+
+add_time("3:00 PM", "3:10")
+
+add_time("11:30 AM", "2:32", "Monday")
+
+add_time("11:43 AM", "00:20")
+
+add_time("10:10 PM", "3:30")
+
+add_time("11:43 PM", "24:20", "tueSday")
+
+add_time("6:30 PM", "205:12")
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -1,54 +1,77 @@
-def add_time(a, b, day=None):
-    t = a.split()
-    days = [
-        "Monday"
-        "Tuesday"
-        "Wednesday"
-        "Thursday"
-        "Friday"
-        "Saturday"
-        "Sunday"
-    ]
-    #print(len(tl))
-    #print(t[0], t[1])
-    t1 = (t[0]).split(':')
-    #print(t1)
-    hrs = int(t1[0])
-    mins = int(t1[1])
-    t2 = b.split(':')
-    #print(t2)
-    hr = int(t2[0])
-    min = int(t2[1])
-    c = hr//24
-    if t[1] == 'PM':
-        hrs += 12
-    if hr >= 24:
-        hr = hr%24
+def add_time(start, duration, starting_day=""):
+    pieces = start.split()
+    time = pieces[0].split(":")
+    ampm = pieces[1]
+
+    duration_pieces = duration.split(":")
     
-    h = hrs + hr
+    if ampm == "PM":
+        time[0] = int(time[0]) + 12
+        
+    ending_h = int(time[0]) + int(duration_pieces[0])
+    ending_min = int(time[1]) + int(duration_pieces[1])
 
-    if h > 24:
-        h -= 12
-    if h > 12:
-        hour = h - 12
-        t[1] = 'PM'
+    days_add = 0
+    
+    if ending_min >= 60:
+        hours_add = ending_min // 60
+        ending_min -= hours_add * 60
+        ending_h += hours_add
+    
+    if ending_h > 24:
+        days_add = ending_h // 24
+        ending_h -= days_add * 24
+    
+    if ending_h > 12:
+        ending_h -= 12
+        ampm = "PM"
+    elif ending_h == 12:
+        ampm = "PM"
+    elif ending_h > 0 and ending_h < 12:
+        ampm = "AM"
+    else: #ending_h == 0
+        ampm = "AM"
+        ending_h += 12
+    
+    if days_add > 0:
+        if days_add == 1:
+            days_later = " (next day)"
+        else:
+            days_later = " (" + str(days_add) + " days later)"
     else:
-        hour = h
-        t[1] = 'AM'
-    print(hour)
-    print(t[1])
-    minute = mins + min
-    if minute > 60:
-        minute -= 60
-        hour += 1
-    print(minute)
-    #print(c)
+        days_later = ""
+    
+    weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                'Friday', 'Saturday', 'Sunday')
+    
+    if starting_day:
+        b = int(days_add) + weekdays.index(starting_day.title())
+        c = b // 7
+        b = b - c * 7
+        end_day = str(", " + weekdays[b])
+    else:
+        end_day = starting_day
+    
+    new_time = str(ending_h) + ":" + \
+        (str(ending_min) if ending_min > 9 else ("0" + str(ending_min))) + \
+        " " + ampm + "" + end_day + days_later
+        
+    print(new_time)
 
-    if c == 1:
-        m = "The next day"
-    elif c > 1:
-        c = c + 1
-        m = f"{c} days later"
-    print(m)
+add_time("3:00 PM", "3:10")
+# Returns: 6:10 PM
 
-add_time("6:12 PM", "205:30")
+add_time("11:30 AM", "2:32", "Monday")
+# Returns: 2:02 PM, Monday
+
+add_time("11:43 AM", "00:20")
+# Returns: 12:03 PM
+
+add_time("10:10 PM", "3:30")
+# Returns: 1:40 AM (next day)
+
+add_time("11:43 PM", "24:20", "tueSday")
+# Returns: 12:03 AM, Thursday (2 days later)
+
+add_time("6:30 PM", "205:12")
+# Returns: 7:42 AM (9 days later)
